5130eccff24db6363d770655838e69bc69ae3a69
Kevin Charm <kevin@kevincharm.com>
2024-05-20 17:36:49 +0200

    fix deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,37 +6,29 @@ const TOKEN_ADDRESS = '0xad5c50fafe2b971d98f37a07c8a532381cce3a0b'
 
 async function main() {
     const [deployer] = await ethers.getSigners()
-    // const booster = await new Booster__factory(deployer)
-    //     .deploy(ANYRAND, TOKEN_ADDRESS, [
-    //         {
-    //             rarity: 0,
-    //             amount: 4,
-    //         },
-    //         {
-    //             rarity: 1,
-    //             amount: 1,
-    //         },
-    //     ])
-    //     .then((tx) => tx.waitForDeployment())
-    // console.log(`Deployed Booster at: ${await booster.getAddress()}`)
+    const args: Parameters<Booster__factory['deploy']> = [
+        ANYRAND,
+        TOKEN_ADDRESS,
+        [
+            {
+                rarity: 0,
+                amount: 4,
+            },
+            {
+                rarity: 1,
+                amount: 1,
+            },
+        ],
+    ]
+    const booster = await new Booster__factory(deployer)
+        .deploy(...args)
+        .then((tx) => tx.waitForDeployment())
+    console.log(`Deployed Booster at: ${await booster.getAddress()}`)
 
     await new Promise((resolve) => setTimeout(resolve, 10_000))
     await run('verify:verify', {
-        address: '0xa5D2D96CBB2f4fc2b493C60DB31104D77af9bf92',
-        constructorArguments: [
-            ANYRAND,
-            TOKEN_ADDRESS,
-            [
-                {
-                    rarity: 0,
-                    amount: 4,
-                },
-                {
-                    rarity: 1,
-                    amount: 1,
-                },
-            ],
-        ],
+        address: await booster.getAddress(),
+        constructorArguments: args,
     })
 }
 
